3cf16b2945fdcaca3c626cb9662af4a867af182d
Yan <yanvashkevich@gmail.com>
2023-05-19 22:51:54 +0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [newItems, setNewItems] = useState([]);
+
+  const addTodo = (e) => {
+    if(!item){
+      alert('Please, enter your todo!!')
+      return;
+    }
+    e.preventDefault()
+
+    const newItem = {
+      value: item
+    };
+
+    setNewItems((oldList) => [...oldList, newItem]);
+    setItem("");
+  }
+
+  function deleteTodo (value){
+    const newArray = newItems.filter(item => item.value !== value)
+
+    setNewItems(newArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <h1 className="title">
+          ToDo List
+        </h1>
+
+
+{/* Все кроме title */}
+      <div className="input-btn-output">
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter your todo please..."
+          value={item}
+          onChange={e => setItem(e.target.value)}
+        />
+        <button onClick={addTodo}>
+          Add
+        </button>
+        <ul className="list">
+          {newItems.map(item => {
+            return <li>{item.value} <button onClick={() => {deleteTodo(item.value)}}>Delete</button></li>;
+          })}
+        </ul>
+      </div>
+      </form>
     </div>
   );
 }
